0c7f3f480f7ac6de774f6f5eddc29fee9d210e2e
RoseFactory <1547892user@naver.com>
2025-10-27 13:12:33 +0900

    feat: 사용자 입력을 List<Car>와 int로 반환하는 메소드 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,8 +2,12 @@ package racingcar;
 
 public class Car {
 
-    private String name;
+    private final String name;
     private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
 
     public void goForward() {
         position++;
--- src/main/java/racingcar/util/InputConverter.java
+++ src/main/java/racingcar/util/InputConverter.java
@@ -1,14 +1,24 @@
 package racingcar.util;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
 import java.util.List;
 import racingcar.Car;
 
 public class InputConverter {
+
     public static List<Car> parseCarNames() {
-        return null;
+        String input = Console.readLine();
+
+        List<String> nameList = Arrays.asList(input.split(","));
+        return nameList.stream()
+                       .map(Car::new)
+                       .toList();
     }
 
     public static int getInt() {
-        return 0;
+        String input = Console.readLine();
+
+        return Integer.parseInt(input);
     }
 }
